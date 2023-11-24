e6d6da027d39b3a95eaf7ea0ebd1c4610e126f7a
Artur Remizov <arturremizov@gmail.com>
2023-11-24 20:45:54 +0300

    Integral Data Types, sizeof, limit

---
+++ 2.integral.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <climits>
+
+using std::cout;
+
+int main() 
+{
+    short a;
+    int b;
+    long c;
+    long long d;
+    unsigned short aa;
+    unsigned int bb;
+    unsigned long cc;
+    unsigned long long dd;
+
+    cout << SHRT_MAX << std::endl;
+    cout << SHRT_MIN << std::endl;
+    cout << ULLONG_MAX << std::endl;
+}
