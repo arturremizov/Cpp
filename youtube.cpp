6ee7baf3ded208bab6fb439db2c892e839d49f9f
Artur Remizov <arturremizov@gmail.com>
2023-11-24 20:11:49 +0300

    Creating Custom Functions

--- youtube.cpp
+++ youtube.cpp
@@ -5,6 +5,16 @@
 using std::cout;
 using std::cin;
 
+double power(double base, int exponent) 
+{
+    double result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        result = result * base;
+    }
+    return result;
+}
+
 int main() 
 {
     int base, exponent;
@@ -13,6 +23,7 @@ int main()
     cout << "What is the exponent? ";
     cin >> exponent;
 
-    double power = pow(base, exponent);
-    cout << power << std::endl;
+    double myPower = power(base, exponent);
+    // double power = pow(base, exponent);
+    cout << myPower << std::endl;
 }
