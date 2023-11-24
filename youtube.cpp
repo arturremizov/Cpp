ea5c0965630fca805f0ad1e68a359b603e819ce3
Artur Remizov <arturremizov@gmail.com>
2023-11-24 20:03:59 +0300

    Pow Function

--- youtube.cpp
+++ youtube.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 // using namespace std; //using directive
 using std::cout;
@@ -6,8 +7,12 @@ using std::cin;
 
 int main() 
 {
-    int slices;
-    cout << "YO fatty how many pieces of pizza you eat?: ";
-    cin >> slices;
-    cout << "You have " << slices << " slices of pizza." << std::endl;
+    int base, exponent;
+    cout << "What is the base? ";
+    cin >> base;
+    cout << "What is the exponent? ";
+    cin >> exponent;
+
+    double power = pow(base, exponent);
+    cout << power << std::endl;
 }
