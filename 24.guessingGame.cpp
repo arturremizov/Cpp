1a005fcdeddeaceb3a3e4f0caabd0773afa4a962
Artur Remizov <arturremizov@gmail.com>
2023-11-26 18:40:30 +0300

    Creating a Menu

---
+++ 24.guessingGame.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() 
+{
+    int choice;
+    do 
+    {
+        std::cout << "0. Quit" << std::endl << "1. Play Game\n"; 
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case 0:
+                std::cout << "Thanks for nothing\n";
+                return 0;
+            case 1:
+              std::cout << "Yo let's play\n";
+                break;
+        } 
+    }
+    while (choice != 0);
+}
