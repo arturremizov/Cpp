0a3a5d11ade501eebd932331355e4a59ea7ded05
Artur Remizov <arturremizov@gmail.com>
2023-11-28 20:14:20 +0300

    Const Modifier

---
+++ 35.constModifier.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void print_array(const int data[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << data[i] << "\t";
+    }
+    std::cout << "\n";
+}
+
+int main() 
+{
+    int data[] = {1, 2, 3};
+    print_array(data, 3);
+    std::cout << data[0] << std::endl;
+}
